3cd9c5c5c1661102762bf06fe1de9ed1bb86814f
Abner Ambrocio <157776355+Ghosyyy@users.noreply.github.com>
2024-04-01 17:09:12 -0600

    tarea - 1 de abril.cpp

--- tarea - 1 de abril.cpp
+++ tarea - 1 de abril.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 int main() {
-    int nums = -1;
-    while (nums <= 0) {
-        cout << "Pon un numero para ver la serie fibonacci aaa: ";
-        cin >> nums;
+    int nums;
+    cout << "Pon un numero para ver la serie fibonacci aaaa: ";
+    cin >> nums;
+
+    if (nums <= 0) {
+        return 0;
     }
 
     int n1 = 0, n2 = 1;
     for (int i = 0; i < nums; ++i) {
         cout << (i == 0 ? n1 : n2) << (i < nums - 1 ? ", " : "");
-        if (i > 0) {
-            n2 += n1;
-            n1 = n2 - n1;
-        }
+        int temp = n2;
+        n2 += n1;
+        n1 = temp;
     }
     return 0;
 }
